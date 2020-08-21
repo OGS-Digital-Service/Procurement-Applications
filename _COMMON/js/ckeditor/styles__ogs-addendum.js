f9c43d32fa54e13b9ba04a7e064b6e958e953008
Jeff HS K <58082485+knaackbuilt@users.noreply.github.com>
2020-08-21 16:29:19 -0400

    wysiwyg styling for PAWS

--- _COMMON/js/ckeditor/styles__ogs-addendum.js
+++ _COMMON/js/ckeditor/styles__ogs-addendum.js
@@ -3,6 +3,10 @@ CKEDITOR.stylesSet.add( 'my_styles', [
     { name: 'Heading 1', element: 'h1', styles: { 'color': 'Blue' } },
     { name: 'Heading 2' , element: 'h2', styles: { 'color': 'Red' } },
 
+    // Object styles
+    { name: 'image', element: 'img', styles: { 'color': 'Blue' } },
+    { name: 'link' , element: 'a', styles: { 'color': 'Red' } },
+
     // Inline styles
     { name: 'CSS Style', element: 'span', attributes: { 'class': 'my_style' } },
     { name: 'Marker: Yellow', element: 'span', styles: { 'background-color': 'Yellow' } }
